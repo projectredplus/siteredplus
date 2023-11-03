4f6886c996af00faa012f6f450416340cb8f0697
Renan <alvesrenanpro@gmail.com>
2023-11-03 12:06:23 -0300

    feat: website with new features and fixes

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+const yearSelect = document.getElementById("yearSelect");
+const cardContainer = document.getElementById("cardContainer");
+
+yearSelect.addEventListener("change", function() {
+    cardContainer.innerHTML = ""; // Limpa os cards anteriores
+
+    const selectedYear = yearSelect.value;
+    const numCards = 3; // Número de redações para exibir
+
+    for (let i = 1; i <= numCards; i++) {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.setAttribute("data-year", selectedYear); // Define o atributo data-year
+        
+        const cardImage = document.createElement("img");
+        cardImage.src = "caderno.png"; // Substitua pelo caminho da imagem do caderno
+        card.appendChild(cardImage);
+
+        const buttonText = document.createTextNode("Ver Redação");
+        const cardButton = document.createElement("button");
+        cardButton.appendChild(buttonText);
+        
+        card.appendChild(cardButton);
+        cardContainer.appendChild(card);
+    }
+    
+    // Referência aos cards criados
+    const cards = cardContainer.querySelectorAll(".card");
+    
+    yearSelect.addEventListener("change", function() {
+        const selectedYear = yearSelect.value;
+    
+        cards.forEach(card => {
+            const cardYear = card.getAttribute("data-year");
+            if (cardYear === selectedYear || selectedYear === "all") {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    });
+});
+
